5d9a3a3b74f1c98c6b212f1f480a887f108ae99b
David Warring <david.warring@gmail.com>
2017-03-20 17:22:40 +1300

    tweak C data-types

--- libpdf/buf.c
+++ libpdf/buf.c
@@ -3,8 +3,8 @@
 #include <libpdf/buf.h>
 
 extern void pdf_buf_pack_8_4(uint8_t *out, uint8_t *in, size_t in_len) {
-  int i;
-  int j = 0;
+  size_t i;
+  size_t j = 0;
   for (i = 0; i < in_len; i++) {
     uint8_t v = in[i];
     out[j++] = v >> 4;
