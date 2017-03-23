fb5393bd87d837808176e044df7d95f1aa35e93e
David Warring <david.warring@gmail.com>
2017-03-24 12:25:51 +1300

    add some prototypes

--- libpdf/buf.h
+++ libpdf/buf.h
@@ -1,8 +1,12 @@
-/* repack from m-byte to n-byte unsigned integers
-   note: 'out' buffer should be m*n*in_len bytes */
+/* repack from m-byte to n-byte unsigned integers */
+extern void pdf_buf_pack_8_1(uint8_t *in, uint8_t *out, size_t in_len);
+extern void pdf_buf_pack_8_2(uint8_t *in, uint8_t *out, size_t in_len);
 extern void pdf_buf_pack_8_4(uint8_t *in, uint8_t *out, size_t in_len);
 extern void pdf_buf_pack_8_16(uint8_t *in, uint16_t *out, size_t in_len);
 extern void pdf_buf_pack_8_32(uint8_t *in, uint32_t *out, size_t in_len);
+
+extern void pdf_buf_pack_1_8(uint8_t *in, uint8_t *out, size_t in_len);
+extern void pdf_buf_pack_2_8(uint8_t *in, uint8_t *out, size_t in_len);
 extern void pdf_buf_pack_4_8(uint8_t *in, uint8_t *out, size_t in_len);
 extern void pdf_buf_pack_16_8(uint16_t *in, uint8_t *out, size_t in_len);
 extern void pdf_buf_pack_32_8(uint32_t *in, uint8_t *out, size_t in_len);
