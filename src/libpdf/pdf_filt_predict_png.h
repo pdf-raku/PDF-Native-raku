0edb799371b4b8e14dc4f981c5b67878f94c0241
David Warring <david.warring@gmail.com>
2017-03-31 06:31:53 +1300

    make all before test etc

--- src/libpdf/pdf_filt_predict_png.h
+++ src/libpdf/pdf_filt_predict_png.h
@@ -10,13 +10,14 @@ pdf_filt_predict_png_decode(uint8_t *buf,
                             uint16_t rows
                             );
 
-extern void pdf_filt_predict_png_encode(uint8_t *buf,
-                                        uint8_t *out,
-                                        uint8_t colors,
-                                        uint8_t bpc,
-                                        uint16_t columns,
-                                        uint16_t rows,
-                                        uint8_t predictor
-                                        );
+extern void
+pdf_filt_predict_png_encode(uint8_t *buf,
+                            uint8_t *out,
+                            uint8_t colors,
+                            uint8_t bpc,
+                            uint16_t columns,
+                            uint16_t rows,
+                            uint8_t predictor
+                            );
 
 #endif
