0d3a8d601a86c7f677e4dacdb5b2cbc6fd4bbc83
David Warring <david.warring@gmail.com>
2022-06-01 07:27:24 +1200

    fixes for windows

--- src/pdf/write.c
+++ src/pdf/write.c
@@ -127,7 +127,7 @@ DLLEXPORT size_t pdf_write_xref_seg(PDF_XREF xref, PDF_UINT length, PDF_STRING b
       uint64_t gen_num = *(xref++);
       uint8_t type     = *(xref++) ? 'n' : 'f';
 
-      sprintf(entry, "%010ld %05ld %c \n", offset, gen_num, type);
+      sprintf(entry, "%010lld %05lld %c \n", offset, gen_num, type);
       concat(&buf_p, buf_end, entry);
   }
 
